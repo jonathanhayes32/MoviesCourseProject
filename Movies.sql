33f3a8c0bce3cd06e3aff5d682009d8316e4bf8e
Jonathan Hayes <jonathan.hayes32@outlook.com>
2018-10-28 23:12:07 -0400

    git commit (add) to command line

---
+++ Movies.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Movies]
+(
+ Movie_id		  INT          NOT NULL IDENTITY,
+  Movie_name      NVARCHAR(40) NULL,
+  Length_minutes  INT NULL,
+  Movie_genre     NVARCHAR(30) NULL,
+  Movie_date	  DATETIME	   NULL,
+  Theatre_id	  INT NULL,
+ CONSTRAINT PK_CP PRIMARY KEY(Movie_id),
+ CONSTRAINT FK_CP_CP FOREIGN KEY(Theatre_id)
+ REFERENCES Theatres(Id)
+);
---
+++ Theatres.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Theatres]
+(
+  Id			  INT          NOT NULL IDENTITY,
+  Address_line    NVARCHAR(60) NOT NULL,
+  Address_city    NVARCHAR(15) NOT NULL,
+  Address_state   NVARCHAR(15) NOT NULL,
+  Address_zip_code NVARCHAR(10) NOT NULL,
+ CONSTRAINT PK_Area PRIMARY KEY(Id)
+);
